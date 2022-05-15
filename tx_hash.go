e0ad2dd9141bf1c1d78c058f4f71ddc311d4d61d
Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
2022-05-15 20:07:22 +0300

    Add comments for methods in tx_hash.go
    Signed-off-by: Gökhan Özeloğlu <gozeloglu@gmail.com>

--- tx_hash.go
+++ tx_hash.go
@@ -4,6 +4,7 @@ import (
 	"time"
 )
 
+// HSet sets field in the hash stored at key to value.
 func (tx *Tx) HSet(key string, field string, value string) (res int, err error) {
 	existVal := tx.HGet(key, field)
 	if existVal == value {
@@ -15,6 +16,8 @@ func (tx *Tx) HSet(key string, field string, value string) (res int, err error) 
 	return
 }
 
+// HGet returns the value associated with field in the hash stored at key. If
+// the key has expired, the key is evicted and empty string is returned.
 func (tx *Tx) HGet(key string, field string) string {
 	if tx.db.hasExpired(key, Hash) {
 		tx.db.evict(key, Hash)
@@ -24,6 +27,8 @@ func (tx *Tx) HGet(key string, field string) string {
 	return toString(tx.db.hashStore.HGet(key, field))
 }
 
+// HGetAll returns all fields and values stored at key. If the key has expired,
+// the key is evicted.
 func (tx *Tx) HGetAll(key string) []string {
 	if tx.db.hasExpired(key, Hash) {
 		tx.db.evict(key, Hash)
@@ -40,8 +45,9 @@ func (tx *Tx) HGetAll(key string) []string {
 	return values
 }
 
-func (tx *Tx) HDel(key string, field ...string) (res int, err error) {
-	for _, f := range field {
+// HDel deletes the fields stored at key.
+func (tx *Tx) HDel(key string, fields ...string) (res int, err error) {
+	for _, f := range fields {
 		e := newRecord([]byte(key), []byte(f), HashRecord, HashHDel)
 		tx.addRecord(e)
 		res++
@@ -49,6 +55,8 @@ func (tx *Tx) HDel(key string, field ...string) (res int, err error) {
 	return
 }
 
+// HKeyExists determines whether the key is exists. If the key has expired, the
+// key is evicted.
 func (tx *Tx) HKeyExists(key string) (ok bool) {
 	if tx.db.hasExpired(key, Hash) {
 		tx.db.evict(key, Hash)
@@ -57,6 +65,8 @@ func (tx *Tx) HKeyExists(key string) (ok bool) {
 	return tx.db.hashStore.HKeyExists(key)
 }
 
+// HExists determines whether the key and field are exists. If the key has
+// expired, the key is evicted.
 func (tx *Tx) HExists(key, field string) (ok bool) {
 	if tx.db.hasExpired(key, Hash) {
 		tx.db.evict(key, Hash)
@@ -66,6 +76,8 @@ func (tx *Tx) HExists(key, field string) (ok bool) {
 	return tx.db.hashStore.HExists(key, field)
 }
 
+// HLen returns number of the fields stored at key. If the key has expired, the
+// key is evicted.
 func (tx *Tx) HLen(key string) int {
 	if tx.db.hasExpired(key, Hash) {
 		tx.db.evict(key, Hash)
@@ -75,6 +87,7 @@ func (tx *Tx) HLen(key string) int {
 	return tx.db.hashStore.HLen(key)
 }
 
+// HKeys returns all fields stored at key. If the key has expired, the key is evicted.
 func (tx *Tx) HKeys(key string) (val []string) {
 	if tx.db.hasExpired(key, Hash) {
 		tx.db.evict(key, Hash)
@@ -84,6 +97,8 @@ func (tx *Tx) HKeys(key string) (val []string) {
 	return tx.db.hashStore.HKeys(key)
 }
 
+// HVals returns all of values stored at key. If the key has expired, the key
+// is evicted.
 func (tx *Tx) HVals(key string) (values []string) {
 	if tx.db.hasExpired(key, Hash) {
 		tx.db.evict(key, Hash)
@@ -98,6 +113,8 @@ func (tx *Tx) HVals(key string) (values []string) {
 	return
 }
 
+// HExpire adds an expiry time for key. If the duration is not positive, expiry
+// time is not set.
 func (tx *Tx) HExpire(key string, duration int64) (err error) {
 	if duration <= 0 {
 		return ErrInvalidTTL
@@ -114,6 +131,7 @@ func (tx *Tx) HExpire(key string, duration int64) (err error) {
 	return
 }
 
+// HTTL returns remaining time for deadline. If the key has expired, the key is evicted.
 func (tx *Tx) HTTL(key string) (ttl int64) {
 	if tx.db.hasExpired(key, Hash) {
 		tx.db.evict(key, Hash)
@@ -127,6 +145,7 @@ func (tx *Tx) HTTL(key string) (ttl int64) {
 	return deadline.(int64) - time.Now().Unix()
 }
 
+// HClear clears the key. If the key has expired, the key is evicted.
 func (tx *Tx) HClear(key string) (err error) {
 	if tx.db.hasExpired(key, Hash) {
 		tx.db.evict(key, Hash)
